036d59085621e7b5b6198157e473f74a54440502
DELL <deshraj9198@gmail.com>
2022-03-08 20:09:24 +0530

    Iniatial Commit

---
+++ app/src/main/java/com/example/tolearn/MainActivity.kt
@@ -0,0 +1,130 @@
+package com.example.tolearn
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.example.tolearn.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val adapter = myFragmentAdapter(supportFragmentManager)
+        binding.content.tabs.setupWithViewPager(binding.content.pager)
+        binding.content.pager.adapter = adapter
+        binding.content.pager.setPageTransformer(true,ZoomOutPageTransformer())
+    }
+    class myFragmentAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm){
+        override fun getCount(): Int {
+            return 9
+        }
+
+        override fun getItem(position: Int): Fragment {
+            return when (position){
+                0 -> HomeFragment()
+                1 -> MercuryFragment()
+                2 -> VenusFragment()
+                3 -> EarthFragment()
+                4 -> MarsFragment()
+                5 -> JupiterFragment()
+                6 -> SaturnFragment()
+                7 -> UranusFragment()
+                else -> NeptuneFragment()
+
+
+
+            }
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return when(position){
+                0 -> "Home"
+                1 -> "1"
+                2 -> "2"
+                3 -> "3"
+                4 -> "4"
+                5 -> "5"
+                6 -> "6"
+                7 -> "7"
+                else -> "8"
+
+            }
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+    class ZoomOutPageTransformer : ViewPager.PageTransformer {
+        private val MIN_SCALE = 0.85f
+        private val MIN_ALPHA = 0.5f
+
+        override fun transformPage(view: View, position: Float) {
+            view.apply {
+                val pageWidth = width
+                val pageHeight = height
+                when {
+                    position < -1 -> { // [-Infinity,-1)
+                        // This page is way off-screen to the left.
+                        alpha = 0f
+                    }
+                    position <= 1 -> { // [-1,1]
+                        // Modify the default slide transition to shrink the page as well
+                        val scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position))
+                        val vertMargin = pageHeight * (1 - scaleFactor) / 2
+                        val horzMargin = pageWidth * (1 - scaleFactor) / 2
+                        translationX = if (position < 0) {
+                            horzMargin - vertMargin / 2
+                        } else {
+                            horzMargin + vertMargin / 2
+                        }
+
+                        // Scale the page down (between MIN_SCALE and 1)
+                        scaleX = scaleFactor
+                        scaleY = scaleFactor
+
+                        // Fade the page relative to its size.
+                        alpha = (MIN_ALPHA +
+                                (((scaleFactor - MIN_SCALE) / (1 - MIN_SCALE)) * (1 - MIN_ALPHA)))
+                    }
+                    else -> { // (1,+Infinity]
+                        // This page is way off-screen to the right.
+                        alpha = 0f
+                    }
+                }
+            }
+        }
+    }
+
+
+}
